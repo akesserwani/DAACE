c15ccd0b4a057ca425d67c341cc607df3b9d8a26
Ali Kesserwani <alikesserwani@Alis-MacBook-Pro.local>
2023-09-26 08:32:28 -0500

    Changed submission box text

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -25,10 +25,11 @@ class MainController < ApplicationController
   def new
     @data_main = DataMain.new
 
-    @text_guide = "<h2>Site Name</h2><br> \n 
+    @text_guide = '<h>Site Name</h><br> \n 
     <p>Site Summary/Details</p><br><hr> \n 
     <h3>New Section</h3><br> \n 
-    <p>Section Details</p><br> "
+    <p>Section Details</p><br><hr> \n 
+    <a href = '' target= '_blank'> Link </a> '
 
   end
   
